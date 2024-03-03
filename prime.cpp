6d3bbfd7a856ce380d9ee1a0460cd91e591b61c7
Girish <girishh.bora@gmail.com>
2024-03-03 13:32:47 +0530

    isPrime

---
+++ prime.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+
+bool isPrime(int n){
+    /*if(n==1)
+        return 0;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0)
+            return 0;
+    }
+    return 1;*/
+    if(n==1)
+        return 0;
+    if(n==2||n==3)
+        return 1;
+    if(n%2==0||n%3==0)
+        return 0;
+    for(int i=5;i*i<=n;i=i+6)
+        if(n%i==0||n%(i+2)==0)
+            return 0;
+    return 1;
+}
+
+int main(){
+    std::cout<<"Enter: ";
+    int n;
+    std::cin>>n;
+    std::cout<<isPrime(n); //1-prime 0-composite
+    return 0;
+}

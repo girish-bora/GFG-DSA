4a7f4052160010a7424e9a13cfa9739f15d0596a
Girish <girishh.bora@gmail.com>
2024-03-16 14:41:52 +0530

    remove duplicate

---
+++ removeDuplicate.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int removeDuplicate(int arr[], int n)
+{   
+    int res=1;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=arr[res-1]){
+            arr[res]=arr[i];
+            res++;
+        }
+    }
+    return res;
+}
+
+int main(){
+    std::cout<<"Enter: ";
+    int n;
+    std::cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        std::cin>>arr[i];
+    }
+    std::cout<<removeDuplicate(arr,n);
+}

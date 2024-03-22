6e0cd0fe8c351a03b55ac8458dc7198836edbf83
Girish <girishh.bora@gmail.com>
2024-03-22 15:44:59 +0530

    last occurence in binary search

---
+++ lastIndex.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int firstOccurence(int arr[], int n,int k)
+{
+    int start=0,end=n-1;
+    while(start<=end){
+        int mid=(start+end)/2;
+        if(k>arr[mid]){
+            start=mid+1;
+        }
+        else if(k<arr[mid]){
+            end=mid-1;
+        }
+        else{
+            if(mid==n-1||arr[mid]!=arr[mid+1])
+                return mid;
+            else
+                start=mid+1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    std::cout<<"Enter: ";
+    int n,k;
+    std::cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        std::cin>>arr[i];
+    }
+    std::cout<<firstOccurence(arr,n,k);
+}
